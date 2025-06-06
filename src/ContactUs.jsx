9c60a0e9f2a13aa1e4fd3ac3271b6b51d051afbf
MohammadAminSalehiVand <mohammad00ezio@gmail.com>
2025-06-06 19:18:54 +0330

    final commit

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -10,7 +10,7 @@ const ContactUs = () => {
         <div className="contactUsBox flex flex-row flex-wrap justify-around">
             <div className="box1 boxes flex flex-row w-full mt-10 mb-10 bg-gray-300 p-10 border-4 border-blue-600 rounded-sm">
                 <img src="./src/photos/phoneIcone.png" className='w-2xs'/>
-                <p className=" text-6xl self-center ml-10 font-extrabold">021-11111111</p>
+                <p className=" text-5xl self-center ml-10 font-extrabold">021-11111111</p>
             </div>
             <div className="box2 boxes flex flex-row w-full mt-10 mb-10 bg-gray-300 p-10 border-4 border-blue-600 rounded-sm">
                 <img src="./src/photos/messageIcone.png" className='w-2xs'/>
